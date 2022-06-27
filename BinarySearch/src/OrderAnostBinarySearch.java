09968707f9f2420e6ecb987731e4b1a63726eff8
TheAmincoder <mdaminmohiuddin@gmail.com>
2022-06-27 12:40:48 +0530

    aded binary Search

---
+++ BinarySearch/src/OrderAnostBinarySearch.java
@@ -0,0 +1,37 @@
+public class OrderAnostBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 20, 36, 65, 74, 85, 89, 90, 95};
+        int target = 20;
+        int ans = oderAgnos(arr, target);
+        System.out.println(ans);
+    }
+
+    static int oderAgnos(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (target > arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
